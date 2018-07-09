37ae86154ea8438f8581d1f4e3d36a4021947054
Daniel Ran <danielr1735@gmail.com>
2018-07-09 17:27:53 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,12 +1,5 @@
 class Costume < ActiveRecord::Base
 
-  def initialize(name, price, size, image_url)
-    @name = name
-    @price = price
-    @size = size
-    @image_url = image_url
-  end
-
 end
 
 # Create your Costume class here
