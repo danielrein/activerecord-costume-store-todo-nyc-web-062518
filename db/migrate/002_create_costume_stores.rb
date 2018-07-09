e8f72513b825a07d43315b2058445e932271456c
Daniel Ran <danielr1735@gmail.com>
2018-07-09 17:42:22 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamps :opening_date
-      t.timestamps :closing_date
+      t.string :opening_date
+      t.string :closing_date
       t.string :description
     end
   end
